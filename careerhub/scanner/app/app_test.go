e7f5871dfd024b810c4e6d89cf5e61ce4026383b
jyo <jyo.liar@gmail.com>
2024-03-13 12:50:59 +0900

    테스트코드 수정

--- careerhub/scanner/app/app_test.go
+++ careerhub/scanner/app/app_test.go
@@ -13,15 +13,17 @@ func TestCheckSkillRequirement(t *testing.T) {
 		Site:           "jumpit",
 		PostingId:      "123",
 		Title:          "Java 개발자",
+		MainTask:       "Java를 이용한 서버 개발, aws 환경에서 서버 구축",
 		Qualifications: "Java 3년 이상, Spring Boot 2년 이상",
 		Preferred:      "Kotlin 1년 이상",
 		RequiredSkill:  []string{},
 	}
 
-	require.True(t, app.CheckSkillRequirement(jobPosting, "kotlin"))
-	require.True(t, app.CheckSkillRequirement(jobPosting, "java"))
-	require.True(t, app.CheckSkillRequirement(jobPosting, "spring boot"))
-	require.False(t, app.CheckSkillRequirement(jobPosting, "javascript"))
-	require.False(t, app.CheckSkillRequirement(jobPosting, "typescript"))
-	require.False(t, app.CheckSkillRequirement(jobPosting, "springboot"))
+	require.Equal(t, *app.CheckSkillRequirement(jobPosting, "kotlin"), app.FromPreferred)
+	require.Equal(t, *app.CheckSkillRequirement(jobPosting, "java"), app.FromTitle)
+	require.Equal(t, *app.CheckSkillRequirement(jobPosting, "spring boot"), app.FromQualifications)
+	require.Equal(t, *app.CheckSkillRequirement(jobPosting, "aws"), app.FromMainTask)
+	require.Nil(t, app.CheckSkillRequirement(jobPosting, "javascript"))
+	require.Nil(t, app.CheckSkillRequirement(jobPosting, "typescript"))
+	require.Nil(t, app.CheckSkillRequirement(jobPosting, "springboot"))
 }
